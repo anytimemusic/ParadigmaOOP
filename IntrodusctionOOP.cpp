9c00105f8e103b1520fa74193bd2872acc2575d7
anytimemusic <bima220711@gmail.com>
2026-04-21 17:35:20 +0700

    class mahasiswa

--- IntrodusctionOOP.cpp
+++ IntrodusctionOOP.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData() {
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
+    
